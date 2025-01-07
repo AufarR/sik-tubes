cc2650d8683d2bcbeaf04a6bfaf93653590b81c9
Wida Inaya Sekar Helia <wida.inaya@gmail.com>
2025-01-08 00:37:51 +0700

    Fetch data edit

--- farmasi/edit_obat.php
+++ farmasi/edit_obat.php
@@ -30,20 +30,43 @@ if ($_SESSION['role'] != 'farmasi') {
   </header>
 
   <main class="main-content">
+
+<?php
+
+include_once("../lib/connection.php");
+$conn = connectDB();
+
+// Prepare and bind parameters
+$stmt = $conn->prepare("SELECT * FROM obat WHERE id=?");
+$stmt->bind_param("i", $_GET["id"]);
+$stmt->execute();
+$stmt->bind_result($id, $nama, $tgl_exp);
+
+// Fetch data & print HTML
+while ($stmt->fetch()) {
+    echo '
     <form action="proses_edit_obat.php" method="post" class="edit-form">
-      <div class="form-group">
-        <label for="nama-obat">Nama Obat</label>
-        <input type="text" id="nama-obat" name="nama-obat" required>
-      </div>
-      <div class="form-group">
-        <label for="kedaluarsa">Tanggal Kedaluarsa</label>
-        <input type="date" id="kedaluarsa" name="kedaluarsa" required>
-      </div>
-      <div class="button-group">
-        <button type="submit" class="btn-submit">Simpan</button>
-        <button type="button" class="btn-cancel" onclick="window.history.back()">Batal</button>
-      </div>
+        <input type="hidden" name="id" value="' . $id .'">
+        <div class="form-group">
+            <label for="nama-obat">Nama Obat</label>
+            <input type="text" id="nama" name="nama" value="' . $nama . '" required>
+        </div>
+        <div class="form-group">
+            <label for="kedaluarsa">Tanggal Kedaluarsa</label>
+            <input type="date" id="kedaluarsa" name="tgl_exp" value="'.$tgl_exp.'" required>
+        </div>
+        <div class="button-group">
+            <button type="submit" class="btn-submit">Simpan</button>
+            <button type="button" class="btn-cancel" onclick="window.history.back()">Batal</button>
+        </div>
     </form>
+    ';
+}
+
+$stmt->close();
+$conn->close();
+
+?>
   </main>
 </body>
 </html>
