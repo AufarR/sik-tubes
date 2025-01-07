2dd30b927b8d63a7c575155bf8cdfe43c5ffec63
Wida Inaya Sekar Helia <wida.inaya@gmail.com>
2025-01-08 00:46:29 +0700

    Update web title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>RSIA Grha Bunda</title>
+  <title>Puskesmas Cinta Kasih Satu Hati</title>
   <link rel="stylesheet" href="styles.css">
   <script>
     // Fungsi untuk mengarahkan ke halaman login
